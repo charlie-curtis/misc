46b882d61fa70dc14f6876b24b0fee8cc654d62a
Charlie Curtis <charlie-curtis@outlook.com>
2016-08-13 17:02:31 -0400

    pass in desired game to plot

--- twitch_stats/plotter.py
+++ twitch_stats/plotter.py
@@ -1,18 +1,32 @@
+import sys
 import plotly as py
 import plotly.graph_objs as go
+from datetime import datetime
 
-# Create random data with numpy
+game = sys.argv[1]
+file = game + ".txt"
+times = []
+viewers = []
+with open(file, "r+") as f:
+	lines = f.readlines()
 
-random_x = [1,2,3,4,5];
-random_y = [1,2,3,4,15];
-
+for i in lines:
+	split_string = i.split(",")
+	split_string[0] = split_string[0].strip()
+	date = datetime.fromtimestamp(float(split_string[0]))
+	times.append(date)
+	print(date)
+	viewers.append(split_string[1].strip())
+	
+print(times)
+print(viewers)
 # Create a trace
 trace = go.Scatter(
-    x = random_x,
-    y = random_y
+    x = times,
+    y = viewers 
 )
 
 data = [trace]
 
-py.offline.plot(data, filename='basic-line')
+py.offline.plot(data, filename=game + ".html")
 
