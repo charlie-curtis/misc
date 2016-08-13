2c11fb5b8ee366b8c6883312edf93a278f7a0949
Charlie Curtis <charlie-curtis@outlook.com>
2016-08-13 17:38:03 -0400

    graph multiple games

--- twitch_stats/plotter.py
+++ twitch_stats/plotter.py
@@ -3,30 +3,33 @@ import plotly as py
 import plotly.graph_objs as go
 from datetime import datetime
 
-game = sys.argv[1]
-file = game + ".txt"
-times = []
-viewers = []
-with open(file, "r+") as f:
-	lines = f.readlines()
+def remove_underscores(str):
+	return str.replace("_", " ")
 
-for i in lines:
-	split_string = i.split(",")
-	split_string[0] = split_string[0].strip()
-	date = datetime.fromtimestamp(float(split_string[0]))
-	times.append(date)
-	print(date)
-	viewers.append(split_string[1].strip())
+
+games = sys.argv[1:]
+data = []
+for game in games:
+	file = game + ".txt"
+	times = []
+	viewers = []
+	with open(file, "r+") as f:
+		lines = f.readlines()
+
+	for i in lines:
+		split_string = i.split(",")
+		split_string[0] = split_string[0].strip()
+		date = datetime.fromtimestamp(float(split_string[0]))
+		times.append(date)
+		viewers.append(split_string[1].strip())
 	
-print(times)
-print(viewers)
-# Create a trace
-trace = go.Scatter(
-    x = times,
-    y = viewers 
-)
+	# Create a trace
+	trace = go.Scatter(
+		x = times,
+		y = viewers,
+		name = remove_underscores(game)
+	)
+	data.append(trace)
 
-data = [trace]
-
-py.offline.plot(data, filename=game + ".html")
+py.offline.plot(data, filename="statistics.html")
 
