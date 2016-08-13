0a19292b0aef8fbb71c80b3c1240806503c119f1
Charlie Curtis <charlie-curtis@outlook.com>
2016-08-13 15:12:00 -0400

    script for collecting data

--- twitch_stats/collector.py
+++ twitch_stats/collector.py
@@ -1,23 +1,31 @@
+#!/usr/bin/env python3
 import re
+import time 
+import os
 from bs4 import BeautifulSoup as bs
 from urllib import request
-#url = "https://www.google.com"
-#url = "https://www.twitch.tv/directory"
+
+dir = os.getcwd()
+print(dir)
 url = "https://www.twitch.tv/"
 response = request.urlopen(url)
+monitored_games = ["StarCraft II", "Counter-Strike: Global Offensive", "Overwatch", "Dota 2", "League of Legends"]
 if response.getcode() != 200:
-  print("unexpected response status")
-  exit(1)
+	print("unexpected response status")
+	exit(1)
 html = response.read()
 soup = bs(html, 'html.parser')
-f = open('data.txt', 'r+');
-
+#find all the game titles
 game_objs = soup.find_all("p", class_="title")
 for game_obj in game_objs:
-  viewers_obj= game_obj.find_next_sibling("p", class_="info")
-  game = game_obj.string.strip()
-  viewers = viewers_obj.string
-  print(viewers);
-  viewers = viewers.replace("viewers", "").strip()
-  print(viewers);
-  f.write(game + ", " + viewers + "\r\n")
+	viewers_obj= game_obj.find_next_sibling("p", class_="info")
+	game = game_obj.string.strip()
+	for looking_for in monitored_games: 
+		if game == looking_for:
+			file = dir + "/" + looking_for.replace(" ", "_") + ".txt"
+			with open(file, 'a+') as f:
+				print("found " + looking_for)
+				viewers = viewers_obj.string.replace(",", "")
+				viewers = viewers.replace("viewers", "").strip()
+				current_time = int(time.time())
+				f.write(str(current_time) + ", " + viewers + "\r\n")
